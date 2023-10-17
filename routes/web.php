8e4c002841949d7d1c99ec7c9cefe2325aca95b5
ianlabicani <labicaniian@gmail.com>
2023-10-17 16:40:51 +0800

    create route for student search

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/student', [StudentController::class, 'index'])->name('student.index');
+Route::get('/student/search', [StudentController::class, 'show'])->name('student.show');
