d8363a39f7436bd3ab1f96881c29bc080965ec71
ianlabicani <labicaniian@gmail.com>
2023-10-18 18:54:17 +0800

    Add routes for session

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
 
 Route::get('/student', [StudentController::class, 'index'])->name('student.index');
-Route::get('/student/search', [StudentController::class, 'show'])->name('student.show');
+Route::get('/student/search', [StudentController::class, 'search'])->name('student.search');
+Route::get('/student/{student}', [StudentController::class, 'show'])->name('student.show');
+Route::get('/session', [SessionController::class, 'index'])->name('session.index');
+Route::get('/session/store', [SessionController::class, 'store'])->name('session.store');
