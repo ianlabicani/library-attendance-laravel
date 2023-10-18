b92bd94af513c3a7663e4f514aa2896902ede025
ianlabicani <labicaniian@gmail.com>
2023-10-18 18:50:26 +0800

    Add script for client-side rendering

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,10 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Library Attendance</title>
+    <link rel="stylesheet" href="{{ asset('assets/style.css') }}">
+    <style>
+    </style>
 </head>
 
 <body>
     {{ $slot }}
 </body>
+<script>
+    let sessionSecElements = document.querySelectorAll(".session-sec");
+
+    function updateSeconds(element) {
+        let initialSeconds = parseInt(element.textContent, 10);
+        initialSeconds++;
+        element.textContent = initialSeconds;
+    }
+
+    sessionSecElements.forEach(function(element) {
+        setInterval(function() {
+            updateSeconds(element);
+        }, 1000);
+    });
+</script>
 
 </html>
