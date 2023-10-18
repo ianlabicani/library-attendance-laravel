0141fd7602763e554534a6bbf71a297bd5e6dac8
ianlabicani <labicaniian@gmail.com>
2023-10-18 18:51:44 +0800

    Add hperlink for a single student

--- resources/views/components/student-card.blade.php
+++ resources/views/components/student-card.blade.php
@@ -1,4 +1,15 @@
 @props(['student'])
-<div>
-    <h1>{{ $student['id'] }}: {{ $student['last_name'] }}, {{ $student['first_name'] }}</h1>
-</div>
+<tr>
+    <td>{{ $student->id }}</td>
+    <td>
+        <a href="{{ route('student.show', ['student' => $student]) }}">{{ $student->last_name }},
+            {{ $student->first_name }} {{ $student->middle_name }}</a>
+    </td>
+    <td>{{ $student->course }}</td>
+    <td>
+        <form action="{{ route('session.store') }}" method="post">
+            @method('get')
+            <button type="submit" name="id" value="{{ $student->id }}">time in</button>
+        </form>
+    </td>
+</tr>
