be8c3186ee65083a38d2f718e43cea151c030295
ianlabicani <labicaniian@gmail.com>
2023-10-18 18:53:12 +0800

    Change variable casing from Camel case to Snake case

--- resources/views/sessions/index.blade.php
+++ resources/views/sessions/index.blade.php
@@ -1,29 +1,29 @@
-<div class="active-session">
-    <table>
-        <tr>
-            <th>Student Id</th>
-            <th>Student ID</th>
-            <th>Student Name</th>
-            <th>Student Session</th>
-            <th>Time Out</th>
-        </tr>
-
-        {{-- $active_sessions = getAllActiveSessions(); --}}
-        // print_r($active_sessions);
-        @foreach ($activeSessions as $activeSession)
+<x-layout>
+    <div class="active-session">
+        <table>
             <tr>
-                <td>{{ $active_session['session_id'] }}</td>
-                <td>{{ $active_session['student_id'] }}</td>
-                <td>{{ $active_session['first_name'] }}</td>
-                <td>{{ "<strong class='session-sec'>" . $active_session['session_seconds'] . '</strong> seconds' }}
-                </td>
-                <td>
-                    {{-- make this a component --}}
-                    {{-- <form action="session.php" method="post">
+                <th>Student Id</th>
+                <th>Student ID</th>
+                <th>Student Name</th>
+                <th>Student Session</th>
+                <th>Time Out</th>
+            </tr>
+            @foreach ($activeSessions as $activeSession)
+                {{-- todo: make this a component --}}
+                <tr>
+                    <td>{{ $activeSession->id }}</td>
+                    <td>{{ $activeSession->student_id }}</td>
+                    <td>{{ $activeSession->first_name }}</td>
+                    <td><strong class='session-sec'>{{ $activeSession->session_seconds }} </strong> seconds
+                    </td>
+                    <td>
+                        {{-- make this a component --}}
+                        {{-- <form action="session.php" method="post">
                         <input type="hidden" name="student_id" value="{{ $student['student_id'] }}">
                         <button type="submit" name="time_out">Time Out</button>
                     </form> --}}
-                </td>
-        @endforeach
-    </table>
-</div>
+                    </td>
+            @endforeach
+        </table>
+    </div>
+</x-layout>
