35f530e7ec1c6a391960489694190940bcd833a0
ianlabicani <labicaniian@gmail.com>
2023-10-18 18:53:54 +0800

    Display more data about student

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -1,1 +1,24 @@
-<h1>{{ $student['id'] }}</h1>
+<x-layout>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Last Name</th>
+                <th>Middle Name</th>
+                <th>First Name</th>
+                <th>Sex</th>
+                <th>Age</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>{{ $student->id }}</td>
+                <td>{{ $student->last_name }}</td>
+                <td>{{ $student->middle_name }}</td>
+                <td>{{ $student->first_name }}</td>
+                <td>{{ $student->sex }}</td>
+                <td>{{ $student->age }}</td>
+            </tr>
+        </tbody>
+    </table>
+</x-layout>
