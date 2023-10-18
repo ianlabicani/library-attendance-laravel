557a8a3402fc27a513e3c39eb8b7a7bee7287bd7
ianlabicani <labicaniian@gmail.com>
2023-10-18 18:50:52 +0800

    Change action route

--- resources/views/components/search-student.blade.php
+++ resources/views/components/search-student.blade.php
@@ -1,4 +1,4 @@
-<form action="{{ route('student.show') }}" method="get">
+<form action="{{ route('student.search') }}" method="get">
     <label for="student_id">Enter Student ID:</label>
     <input type="text" name="id" id="student_id" placeholder="e.g., S001">
     <button type="submit">Search</button>
